c3b3612788ba77c4e2b0b8ea1166cf2313101086
Martin Tornqvist <m.tornq@gmail.com>
2013-06-16 15:08:06 +0200

    asdf

--- src/Config.cpp
+++ src/Config.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 Config::Config(Engine* engine) :
-  GAME_VERSION("v14.0.2"),
+  GAME_VERSION("v14.1"),
   TILES_IMAGE_NAME("images/gfx_16x24.png"),
   MAIN_MENU_LOGO_IMAGE_NAME("images/main_menu_logo.png"),
   fontImageName(""),
