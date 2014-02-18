430f1736c580df77b7beb594858d78c1de01d0ed
Martin Törnqvist <m.tornq@gmail.com>
2014-02-18 08:38:29 +0100

    Corrected spelling error

--- src/PlayerBonuses.cpp
+++ src/PlayerBonuses.cpp
@@ -85,7 +85,7 @@ void PlayerBonHandler::getBgDescr(
     case bgOccultist: {
       linesRef.push_back("Can memorize spells cast from manuscripts");
       linesRef.push_back(" ");
-      linesRef.push_back("-50% shock taken from using and idenfifying ");
+      linesRef.push_back("-50% shock taken from using and identifying ");
       linesRef.push_back("strange items (e.g. potions)");
       linesRef.push_back(" ");
       linesRef.push_back("-2 Hit Points");
