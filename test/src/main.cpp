d31a492769208f6db616148e961afd853a4c3acb
Martin Törnqvist <m.tornq@gmail.com>
2014-02-23 19:20:21 +0100

    Unit tests working again

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -383,7 +383,7 @@ TEST_FIXTURE(BasicFixture, Explosions) {
   //Check damage to actors
   Actor* a1 = eng.actorFactory->spawnActor(actor_rat, Pos(X0 + 1, Y0));
   Explosion::runExplosionAt(Pos(X0, Y0), eng);
-  CHECK_EQUAL(ActorDeadState::destroyed, a1->deadState);
+  CHECK_EQUAL(int(ActorDeadState::destroyed), int(a1->deadState));
 
   //Check that corpses can be destroyed, and do not block living actors
   const int NR_CORPSES = 3;
@@ -395,9 +395,9 @@ TEST_FIXTURE(BasicFixture, Explosions) {
   a1 = eng.actorFactory->spawnActor(actor_rat, Pos(X0 + 1, Y0));
   Explosion::runExplosionAt(Pos(X0, Y0), eng);
   for(int i = 0; i < NR_CORPSES; i++) {
-    CHECK_EQUAL(ActorDeadState::destroyed, corpses[i]->deadState);
+    CHECK_EQUAL(int(ActorDeadState::destroyed), int(corpses[i]->deadState));
   }
-  CHECK_EQUAL(ActorDeadState::destroyed, a1->deadState);
+  CHECK_EQUAL(int(ActorDeadState::destroyed), int(a1->deadState));
 
   //Check explosion applying Burning to living and dead actors
   a1        = eng.actorFactory->spawnActor(actor_rat, Pos(X0 - 1, Y0));
@@ -505,9 +505,9 @@ TEST_FIXTURE(BasicFixture, MonsterStuckInSpiderWeb) {
   }
   //Check that all cases have been triggered (not really necessary, it just
   //verifies that the loop above is correctly written).
-  CHECK_EQUAL(isTestedStuck, true);
-  CHECK_EQUAL(isTestedLooseWebIntact, true);
-  CHECK_EQUAL(isTestedLooseWebDestroyed, true);
+  CHECK(isTestedStuck);
+  CHECK(isTestedLooseWebIntact);
+  CHECK(isTestedLooseWebDestroyed);
 }
 
 TEST_FIXTURE(BasicFixture, SavingGame) {
@@ -633,8 +633,8 @@ TEST_FIXTURE(BasicFixture, LoadingGame) {
   CHECK_EQUAL(7, eng.map->getDlvl());
 
   //Actor data
-  CHECK_EQUAL(
-    123, eng.actorDataHandler->dataList[endOfActorIds - 1].nrOfKills);
+  CHECK_EQUAL(123,
+              eng.actorDataHandler->dataList[endOfActorIds - 1].nrOfKills);
 
   //Learned spells
   PlayerSpellsHandler& spHlr = *(eng.playerSpellsHandler);
