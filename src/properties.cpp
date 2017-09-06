f60af6c165169eff80c932874e5eb93985d4fd7a
Martin Törnqvist <m.tornq@gmail.com>
2017-09-06 18:40:21 +0200

    Reduced default duration of paralysis

--- src/properties.cpp
+++ src/properties.cpp
@@ -353,7 +353,7 @@ void init_data_list()
     add_prop_data(d);
 
     d.id = PropId::paralyzed;
-    d.std_rnd_turns = Range(5, 7);
+    d.std_rnd_turns = Range(3, 5);
     d.name = "Paralyzed";
     d.name_short = "Paralyzed";
     d.descr = "Cannot move";
