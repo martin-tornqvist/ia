10062bd4e4d5e405aa3286c843426509e1c94f87
Martin Törnqvist <m.tornq@gmail.com>
2018-01-10 19:09:16 +0100

    Improved emacs auto-complete/syntax checking
    Generating compile_commands.json database in build directory with
    cmake when running build-debug.sh, instead of using static
    .clang_complete or dir-locals.el files.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,23 +1,23 @@
-((nil
-  . ((eval . (progn
-               (require 'projectile)
-               (setq company-clang-arguments (delete-dups (append
-                                                           company-clang-arguments
-                                                           (list
-                                                            "-std=c++14"
-                                                            (concat "-I" (projectile-project-root) "include")
-                                                            (concat "-I" (projectile-project-root) "rl_utils/include")
-
-                                                            "-I/usr/include/SDL2"
-                                                            ))))))))
+(
+ ;; For all modes
+ (nil
+  . (
+     (indent-tabs-mode . nil)
+     ))
+ ;; For c++ mode
  (c++-mode
-  (whitespace-line-column . 80)
-  (eval ignore-errors
-        (add-hook 'write-contents-functions
-                  (lambda ()
-                    (delete-trailing-whitespace)
-                    nil))
-        (require 'whitespace)
-        (whitespace-mode 0)
-        (whitespace-mode 1)))
+  . (
+     (fill-column . 80)
+     ;; NOTE: This will not have an effect until whitespace-mode is (re)started.
+     (whitespace-line-column . 80)
+     (c-file-style . "bsd")
+     ;; Disable/enable whitespace-mode, see note above.
+     ;; NOTE: This is "unsafe", see:
+     ;; M-x describe-variable -> enable-local-variables
+     ;; This will not be evaluated if enable-local-variables is set to ":safe".
+     (eval ignore-errors
+           (require 'whitespace)
+           (whitespace-mode 0)
+           (whitespace-mode 1)))
+  )
  )
--- build-debug.sh
+++ build-debug.sh
@@ -5,11 +5,8 @@ set -xue
 root_dir=$PWD
 
 mkdir -p build
-
 cd build
-
-cmake ../
-
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
 make -j 4 ia-debug
 
 cd $root_dir
