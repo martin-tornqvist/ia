3c51d0e3450666d94fb0d5aac0f6028563e215cd
Martin Törnqvist <m.tornq@gmail.com>
2018-03-30 00:19:30 +0200

    Revert "Debug build script uses Ninja instead of Make"
    This reverts commit bc6bc627ad530d51770fe363611b4649ebe8b834.

--- build-debug.sh
+++ build-debug.sh
@@ -6,7 +6,7 @@ root_dir=$PWD
 
 mkdir -p build
 cd build
-cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
-ninja ia-debug
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../
+make -j 4 ia-debug
 
 cd $root_dir
